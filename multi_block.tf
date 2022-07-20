11490e03071499f87bafd4f10e8ab4be6a44e497
Shubham Borkar <shubham_borkar@outlook.com>
2022-07-20 14:59:45 +0530

    multi blocks in terraform

---
+++ multi_block.tf
@@ -0,0 +1,11 @@
+output "first_block" {
+  value = "This is first block"
+}
+
+output "second_block" {
+  value = "This is second block"
+}
+
+output "third_block" {
+  value = "This is third block"
+}
