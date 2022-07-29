c867fcbf265d2f841571c8c7dcdc01e41387ced7
Shubham Borkar <shubham_borkar@outlook.com>
2022-07-29 19:12:08 +0530

    functions in terraform

--- function.tf
+++ function.tf
@@ -1,3 +1,12 @@
 //* output "function" {
 //    value = "${function-name(" ")}"
-//}
+// }
+
+//file function:
+output "printfile" {
+    value = file("${path.module}/<file-name>")
+}
+
+//lookup function:
+output "lookup" {
+    value = lookup("${}")
