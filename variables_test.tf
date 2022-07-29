e62d6be26a9722541d16663742fce39d520f72f8
Shubham Borkar <shubham_borkar@outlook.com>
2022-07-29 19:15:56 +0530

    variable examples

--- variables_test.tf
+++ variables_test.tf
@@ -12,7 +12,7 @@ output "fruits-name" {
 
 output "userage-map" {
   value = "The name of the student is ${var.username} and his/her age is ${lookup(var.userage-map, "${var.username}")}"
-  }
+}
 
 // To define variable in non interractive mode or directly in command line mode, do
 //terraform plan -var "username=shubham"  -var "age=28" --var 'users["user1", "user2", "user3"]'
